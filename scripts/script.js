f48b3a480d5e0530f04728ed0447bc5cfc4a9412
italosanches <italo_carvalho157@hotmail.com>
2024-12-12 21:02:03 -0300

    Finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 const botao = document
   .querySelector("#button-default")
   .addEventListener("click", function () {
-    const initialValue = document.querySelector("#number1").value;
-    const finalValue = document.querySelector("#number2").value;
-    let textTabuada = document.querySelector("#numero-tabuada");
+    const multiplicador = +document.querySelector("#multiplicador").value;
+    const multiplicando = +document.querySelector("#multiplicando").value;
+    let spanMultiplicador = document.querySelector(".number-multiplicador").textContent = multiplicador;
     const divResult = document.querySelector("#container-result");
-    textTabuada.textContent = "Tabuada do número:";
-    if (initialValue && finalValue) {
-      while (divResult.firstChild) {
-        divResult.removeChild(divResult.firstChild);
-      } //executa enquanto tiver filho
-      textTabuada.textContent += initialValue;
 
-      for (let i = 1; i <= finalValue; i++) {
-        let paragrafo = document.createElement("p");
-        paragrafo.textContent = `${initialValue} x ${i} = ${initialValue * i}`;
-        paragrafo.setAttribute("class", "p-result");
-        divResult.appendChild(paragrafo);
-      }
+    if (!multiplicador || !multiplicando) {
+      return
+    }   
+    while (divResult.firstChild) {
+      divResult.removeChild(divResult.firstChild);
+    } //executa enquanto tiver filho
+
+    for (let i = 1; i <= multiplicando; i++) {
+      let paragrafo = document.createElement("p");
+      paragrafo.textContent = `${multiplicador} x ${i} = ${multiplicador * i}`;
+      paragrafo.setAttribute("class", "p-result");
+      divResult.appendChild(paragrafo);
     }
   });
