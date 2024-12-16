463ff7f6cadd423e2f8f8047469c85b6ee3435bc
italosanches <italo_carvalho157@hotmail.com>
2024-12-16 17:48:56 -0300

    ajustado classes e js

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,27 @@
-const botao = document
-  .querySelector("#button-default")
-  .addEventListener("click", function () {
-    const multiplicador = +document.querySelector("#multiplicador").value;
-    const multiplicando = +document.querySelector("#multiplicando").value;
-    let spanMultiplicador = document.querySelector(".number-multiplicador").textContent = multiplicador;
-    const divResult = document.querySelector("#container-result");
+document
+	.querySelector("#button-default")
+	.addEventListener("click", function () {
+		const multiplicador = +document.querySelector("#multiplicador").value;
+		const multiplicando = +document.querySelector("#multiplicando").value;
+		if (!multiplicador || !multiplicando) {
+			return;
+		}
+		const divResult = document.querySelector("#container-result");
+		limparTabuada(divResult);
+		criarTabuada(divResult, multiplicador, multiplicando);
+	});
 
-    if (!multiplicador || !multiplicando) {
-      return
-    }   
-    while (divResult.firstChild) {
-      divResult.removeChild(divResult.firstChild);
-    } //executa enquanto tiver filho
-
-    for (let i = 1; i <= multiplicando; i++) {
-      let paragrafo = document.createElement("p");
-      paragrafo.textContent = `${multiplicador} x ${i} = ${multiplicador * i}`;
-      paragrafo.setAttribute("class", "p-result");
-      divResult.appendChild(paragrafo);
-    }
-  });
+function criarTabuada(divResult, multiplicador, multiplicando) {
+	document.querySelector(".number-multiplicador").textContent = multiplicador;
+	for (let i = 1; i <= multiplicando; i++) {
+		let paragrafo = document.createElement("p");
+		paragrafo.textContent = `${multiplicador} x ${i} = ${multiplicador * i}`;
+		paragrafo.setAttribute("class", "p-result");
+		divResult.appendChild(paragrafo);
+	}
+}
+function limparTabuada(divResult) {
+	while (divResult.firstChild) {
+		divResult.removeChild(divResult.firstChild);
+	} //executa enquanto tiver filho
+}
