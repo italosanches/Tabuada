7f67818a41742f783697c40dc78a6d95f3daf93b
italosanches <italo_carvalho157@hotmail.com>
2024-12-18 16:19:36 -0300

    Refatorado codigo para separação de responsabilidades em funções.

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,38 @@
-document
-	.querySelector("#button-default")
-	.addEventListener("click", function () {
-		const multiplicador = +document.querySelector("#multiplicador").value;
-		const multiplicando = +document.querySelector("#multiplicando").value;
-		if (!multiplicador || !multiplicando) {
-			return;
-		}
-		const divResult = document.querySelector("#container-result");
-		limparTabuada(divResult);
-		criarTabuada(divResult, multiplicador, multiplicando);
-	});
+document.querySelector("#button-default").addEventListener("click", createMultiplicationTable);
 
-function criarTabuada(divResult, multiplicador, multiplicando) {
-	document.querySelector(".number-multiplicador").textContent = multiplicador;
-	for (let i = 1; i <= multiplicando; i++) {
+function createMultiplicationTableInHtml(divResult, resultMultiplication, multiplicator, spanMultiplicator) {
+	spanMultiplicator.textContent = multiplicator;
+	resultMultiplication.forEach((e) => {
 		let paragrafo = document.createElement("p");
-		paragrafo.textContent = `${multiplicador} x ${i} = ${multiplicador * i}`;
+		paragrafo.textContent = e;
 		paragrafo.setAttribute("class", "p-result");
 		divResult.appendChild(paragrafo);
-	}
+	});
 }
-function limparTabuada(divResult) {
-	while (divResult.firstChild) {
-		divResult.removeChild(divResult.firstChild);
-	} //executa enquanto tiver filho
+function clearMultiplicationTable(divResult, spanMultiplicator) {
+	divResult.textContent = "";
+	spanMultiplicator.textContent = "";
+	// while (divResult.firstChild) {
+	// 	divResult.removeChild(divResult.firstChild);
+	// } //executa enquanto tiver filho
+}
+function generateMultiplication(multiplicand, multiplicate) {
+	const result = [];
+	for (let i = 1; i <= multiplicand; i++) {
+		result.push(`${multiplicate} x ${i} = ${multiplicate * i}`);
+	}
+	return result;
+}
+
+function createMultiplicationTable() {
+	const divResult = document.querySelector("#container-result");
+	const spanMultiplicator = document.querySelector(".number-multiplicador");
+	clearMultiplicationTable(divResult, spanMultiplicator);
+	const multiplicator = +document.querySelector("#multiplicador").value;
+	const multiplicand = +document.querySelector("#multiplicando").value;
+	if (!multiplicator || !multiplicand) {
+		return;
+	}
+	const resultMultiplication = generateMultiplication(multiplicand, multiplicator);
+	createMultiplicationTableInHtml(divResult, resultMultiplication, multiplicator, spanMultiplicator);
 }
